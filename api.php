58c780fe629cf986b30f4e1c029ca7a6c780c4e7
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-06 16:30:26 -0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,65 @@
+<?php
+    if(isset($_GET['category'])){
+        $array = creacion();// Crie a array com os dados da API
+        $category = $_GET['category'];// Pegue o valor da categoria
+        $series = getValores($array);// Pegue os valores da categoria
+        $series = $series[ucwords($category)];
+    }
+    function creacion(){// Crie a array com os dados da API
+        $url = "http://api.tvmaze.com/shows";
+        $array = json_decode(file_get_contents($url), TRUE);
+        return $array;
+    }
+    function getValores($array){// Pegue os valores da categoria
+        $series = array();// Crie a array com os dados da API
+        for($i = 0; $i < count($array); $i++){// Para cada item da array
+            /*$categoria = $array[$i]['type'];// Pegue o valor da categoria
+            if(!isset($series[$categoria])){// Se a categoria não existir
+                $series[$categoria] = array();// Crie a categoria
+            }
+            array_push($series[$categoria], $array[$i]);// Adicione o item na categoria*/
+            for($j = 0; $j < count($array[$i]['genres']); $j++){// Para cada item da categoria
+                /*$categoria = $array[$i]['genres'][$j];// Pegue o valor da categoria
+                if(!isset($series[$categoria])){// Se a categoria não existir
+                    $series[$categoria] = array();// Crie a categoria
+                }
+                array_push($series[$categoria], $array[$i]);// Adicione o item na categoria*/
+                $image = str_replace('http', 'https', $array[$i]['image']['medium']);// Pegue a imagem
+                $serie = array($array[$i]['name'], $array[$i]['rating']['average'], $image, $array[$i]['id']);// Crie a array com os dados da API
+                $series[$array[$i]['genres'][$j]][] = $serie;// Adicione o item na categoria
+            }
+        }
+        return $series;// Retorne a array com os dados da API
+    }
+    function getValoresIndividual($id){// Pegue os valores da categoria
+        require_once 'serieIndividual.php';// Importe o arquivo
+        require_once 'cast.php';// Importe o arquivo
+        $url = "http://api.tvmaze.com/shows/" . $id . '?embed=cast';// Crie a url
+        $array = json_decode(file_get_contents($url), TRUE);// Crie a array com os dados da API
+        $serieTotal = array();
+        if(count($array) > 0){// Se a array não for vazia
+            /*$serieTotal['name'] = $array['name'];// Pegue o nome
+            $serieTotal['rating'] = $array['rating']['average'];// Pegue a nota
+            $serieTotal['image'] = str_replace('http', 'https', $array['image']['medium']);// Pegue a imagem
+            $serieTotal['id'] = $array['id'];// Pegue o id
+            $serieTotal['summary'] = $array['summary'];// Pegue o resumo
+            $serieTotal['cast'] = getCast($array['_embedded']['cast']);// Pegue o cast*/
+            $image = str_replace('http', 'https', $array['image']['original']);// Pegue a imagem
+            $serie = new Serie($array['name'], $array['rating']['average'], $array['image']['original'], $array['id']);// Crie a array com os dados da API
+            $serie->setSummary($array['summary']);// Pegue o resumo
+            $serie->setLanguage($array['language']);// Pegue o idioma
+            $serie->setType($array['type']);// Pegue o tipo
+            $serie->setGenres($array['genres']);// Pegue o genero
+            $serie->setStatus($array['status']);// Pegue o status
+            $serie->setSchedule($array['schedule']['days'][0] . ' at ' . $array['schedule']['time']);// Pegue o horario
+            for($i = 0; $i < count($array['_embedded']['cast']); $i++){// Para cada item da array
+                /*$serie->addCast(new Cast($array['_embedded']['cast'][$i]['person']['name'], $array['_embedded']['cast'][$i]['character']['name']));// Adicione o item na categoria*/
+                $imageCast = $array['_embedded']['cast'][$i]['person']['image']['medium'];// Pegue a imagem
+                $castIndi = new Cast($array['_embedded']['cast'][$i]['person']['name'], $array['_embedded']['cast'][$i]['character']['name'], $imageCast);// Crie a array com os dados da API
+                $cast[] = $castIndi;// Adicione o item na categoria
+            }
+            $serieTotal[] = $serie;// Adicione o item na categoria
+            $serieTotal[] = $cast;
+        }
+        return $serieTotal;// Retorne a array com os dados da API
+    }
