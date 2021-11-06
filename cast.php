6f651a08524ae44b3043626b85f404e01191b75e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-06 16:35:05 -0300

    Create cast.php

---
+++ cast.php
@@ -0,0 +1,20 @@
+<?php
+    class Cast{
+        private $name;
+        private $alias;
+        private $image;
+        public function __construct($name, $alias, $image){
+            $this->name = $name;
+            $this->alias = $alias;
+            $this->image = $image;
+        }
+        public function getName(){
+            return $this->name;
+        }
+        public function getAlias(){
+            return $this->alias;
+        }
+        public function getImage(){// retorna a url da imagem
+            return $this->image;
+        }
+    }
