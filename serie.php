d8cb1d0bb302ef9651aceac1e60c212e42584a9d
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-11-06 18:19:45 -0300

    Update serie.php

--- serie.php
+++ serie.php
@@ -63,7 +63,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Serie <?= $serie->getTitle() ?></title>
+        <title><?= $serie->getTitle() ?></title>
         <link href="https://fonts.googleapis.com/css?family=Barlow:300|Fira+Sans:500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="css/stylesPageSerie.css">
         <link rel="shortcut icon" href="./images/favicon.png"/>
