151e9f81011a5dbc49fcccda1644d16449aaae8f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-18 14:57:35 -0300

    Update cast.php

--- cast.php
+++ cast.php
@@ -3,9 +3,11 @@
         private $name;
         private $alias;
         private $image;
+        //private $country;
         public function __construct($name, $alias, $image){
             $this->name = $name;
             $this->alias = $alias;
+            //$this->country = $country;
             $this->image = $image;
         }
         public function getName(){
@@ -14,6 +16,9 @@
         public function getAlias(){
             return $this->alias;
         }
+        /*public function getCountry(){
+            return $this->country;
+        }*/
         public function getImage(){// retorna a url da imagem
             return $this->image;
         }
