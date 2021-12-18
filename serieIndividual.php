0578bccfdbaab06522e3585631f08fce7c5e34c7
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2021-12-18 14:57:37 -0300

    Update serieIndividual.php

--- serieIndividual.php
+++ serieIndividual.php
@@ -16,6 +16,7 @@
             $this->star = $star;
             $this->image = $image;
             $this->id = $id;
+            $this->id = $country;
         }
         public function getTitle(){
             return $this->title;
